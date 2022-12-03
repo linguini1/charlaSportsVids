1a089361e00b11b052ba7f65595b201c50ffc886
Matteo Golin <matteo.golin@gmail.com>
2022-12-03 12:16:23 -0500

    GUI now mostly works, but cannot perform the long logic.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ __author__ = "Matteo Golin"
 from conversion import TSVideo
 from web import get_m3u8_link, parse_video_name
 import moviepy.editor as mp
+from gui import GUI
 
 # Constants
 OUTPUT_DIR = "./output"
@@ -17,23 +18,12 @@ CREDENTIALS = {
 # Main
 def main():
 
-    # Ask for URL
-    video_url = input("Video URL: ")
-    CREDENTIALS["email"] = input("OUATV account email: ")
-    CREDENTIALS["password"] = input("OUATV account password: ")
-
-    # Get M3U8
-    m3u8_url = get_m3u8_link(video_url, CREDENTIALS)
-
-    # Save as video.ts
-    video = TSVideo(m3u8_url)
-    video_name = parse_video_name(video_url)
-    file_path = f"{OUTPUT_DIR}/{video_name}"
-    video.save(file_path)
-
-    # Change to mp4
-    clip = mp.VideoFileClip(f"{file_path}.ts")
-    clip.write_videofile(f"{file_path}.mp4")
+    # Create GUI
+    gui = GUI(
+        credentials=CREDENTIALS,
+        output_dir=OUTPUT_DIR
+    )
+    gui.run()
 
 
 if __name__ == '__main__':
